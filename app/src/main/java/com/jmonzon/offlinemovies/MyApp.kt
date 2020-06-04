95540b0468617281630e8b8c926f2f018a101d19
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-06-04 22:38:50 +0200

    Creamos clase MyAPP y seteamos contexto para poder acceder a él desde cualquier parte de la aplicación

---
+++ app/src/main/java/com/jmonzon/offlinemovies/MyApp.kt
@@ -0,0 +1,19 @@
+package com.jmonzon.offlinemovies
+
+import android.app.Application
+import android.content.Context
+
+abstract class MyApp : Application() {
+    companion object {
+
+        private lateinit var context: Context
+
+        fun setContext(con: Context) {
+            context = con
+        }
+
+        fun getContext() : Context {
+            return context
+        }
+    }
+}
