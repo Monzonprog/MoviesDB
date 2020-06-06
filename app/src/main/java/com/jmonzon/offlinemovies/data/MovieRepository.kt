1bb4e41380f46812242c33735010128621e65639
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-06-06 19:25:24 +0200

    Añadimos RequestInteceptor para añadir el ApiKey en nuestras peticiones

--- app/src/main/java/com/jmonzon/offlinemovies/data/MovieRepository.kt
+++ app/src/main/java/com/jmonzon/offlinemovies/data/MovieRepository.kt
@@ -2,6 +2,8 @@ package com.jmonzon.offlinemovies.data
 
 import com.jmonzon.offlinemovies.data.remote.ApiConstant
 import com.jmonzon.offlinemovies.data.remote.MovieApiService
+import com.jmonzon.offlinemovies.data.remote.RequestInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,10 +12,15 @@ class MovieRepository {
     private var movieApiService: MovieApiService
 
     init {
+        //Include API_KEY in query
+        var okHttpClientBuilder: OkHttpClient.Builder = OkHttpClient.Builder().addInterceptor(RequestInterceptor())
+        var client: OkHttpClient = okHttpClientBuilder.build()
+
         //REMOTE -> RETROFIT
         var retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(ApiConstant.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
 
         movieApiService = retrofit.create(MovieApiService::class.java)
