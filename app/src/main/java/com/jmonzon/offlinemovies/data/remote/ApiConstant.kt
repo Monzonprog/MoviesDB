5ac34c0528f3cdd30f4cb24cc19d63bf82e059ff
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-06-06 19:07:28 +0200

    Se añaden constantes

--- app/src/main/java/com/jmonzon/offlinemovies/data/remote/ApiConstant.kt
+++ app/src/main/java/com/jmonzon/offlinemovies/data/remote/ApiConstant.kt
@@ -2,6 +2,8 @@ package com.jmonzon.offlinemovies.data.remote
 
 class ApiConstant {
     companion object{
-        val urlPhotoApi: String = "https://image.tmdb.org/t/p/w500/"
+        const val IMAGE_API_PREFIX: String = "https://image.tmdb.org/t/p/w500/"
+        const val BASE_URL: String = "https://api.themoviedb.org/3/"
+        const val API_KEY: String = "40bd3de9a0cc86442c18ee793d9001d8"
     }
 }
