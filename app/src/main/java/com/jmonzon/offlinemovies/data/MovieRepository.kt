8f3d455d7ae1b3860716758ad70b37eed8261b77
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-06-11 22:25:13 +0200

    Implementamos clase NetworkBoundResource

--- app/src/main/java/com/jmonzon/offlinemovies/data/MovieRepository.kt
+++ app/src/main/java/com/jmonzon/offlinemovies/data/MovieRepository.kt
@@ -1,13 +1,20 @@
 package com.jmonzon.offlinemovies.data
 
+import androidx.annotation.NonNull
+import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.jmonzon.offlinemovies.app.MyApp
 import com.jmonzon.offlinemovies.data.local.MovieRoomDataBase
 import com.jmonzon.offlinemovies.data.local.dao.MovieDao
+import com.jmonzon.offlinemovies.data.local.dao.entity.MovieEntity
+import com.jmonzon.offlinemovies.data.network.NetworkBoundResource
+import com.jmonzon.offlinemovies.data.network.Resource
 import com.jmonzon.offlinemovies.data.remote.ApiConstant
 import com.jmonzon.offlinemovies.data.remote.MovieApiService
 import com.jmonzon.offlinemovies.data.remote.RequestInterceptor
+import com.jmonzon.offlinemovies.data.remote.model.MoviesResponse
 import okhttp3.OkHttpClient
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -40,5 +47,23 @@ class MovieRepository {
         movieApiService = retrofit.create(MovieApiService::class.java)
     }
 
+    fun getPopularMovies(): LiveData<Resource<List<MovieEntity>>> {
+        return object : NetworkBoundResource<List<MovieEntity>, MoviesResponse>() {
+            //Type return by room, type return by Retrofit
+            override fun saveCallResult(@NonNull item: MoviesResponse) {
+                movieDao.saveMovies(item.results)
+            }
 
+            override fun loadFromDb(): LiveData<List<MovieEntity>> {
+                return movieDao.loadMovies()
+            }
+
+            override fun createCall(): Call<MoviesResponse> {
+                return movieApiService.loadPopularMovies()
+            }
+
+        }.asLiveData()
+
+    }
 }
+
