3587552bc59a3eb53ac6128e3111ba4a9d56a5bf
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-06-13 22:23:39 +0200

    Instanciamos recyclerView y subimos el número de columnas a 2 para mejor visualización

--- app/src/main/java/com/jmonzon/offlinemovies/ui/MovieFragment.kt
+++ app/src/main/java/com/jmonzon/offlinemovies/ui/MovieFragment.kt
@@ -12,15 +12,12 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jmonzon.offlinemovies.R
-import com.jmonzon.offlinemovies.app.MyApp
 import com.jmonzon.offlinemovies.data.local.dao.entity.MovieEntity
-import com.jmonzon.offlinemovies.data.network.Status
 import com.jmonzon.offlinemovies.viewModel.MovieViewModel
 
 class MovieFragment : Fragment() {
 
-    // TODO: Customize parameters
-    private var columnCount = 1
+    private var columnCount = 2
     private val ARG_COLUMN_COUNT = "column-count"
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: MyMovieRecyclerViewAdapter
@@ -34,6 +31,8 @@ class MovieFragment : Fragment() {
         arguments?.let {
             columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
+        movieViewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
+
     }
 
     override fun onCreateView(
@@ -41,8 +40,7 @@ class MovieFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_movie_list, container, false)
-        MyApp.setContext(view.context)
-        movieViewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
+        recyclerView = view.findViewById(R.id.list)
         // Set the adapter
         if (view is RecyclerView) {
             with(view) {
@@ -50,8 +48,6 @@ class MovieFragment : Fragment() {
                     columnCount <= 1 -> LinearLayoutManager(context)
                     else -> GridLayoutManager(context, columnCount)
                 }
-                /*adapter = MyMovieRecyclerViewAdapter(context, movieList)
-                recyclerView.adapter = adapter*/
                 loadMovies(context)
             }
         }
@@ -60,11 +56,13 @@ class MovieFragment : Fragment() {
 
     private fun loadMovies(context: Context) {
         movieViewModel.getPopularMovies().observe(viewLifecycleOwner, Observer {
-            if (it.status.name !== Status.LOADING.toString()) {
+            if (it.status.toString() !== "LOADING") {
                 movieList = it.data!!
+                adapter = MyMovieRecyclerViewAdapter(context, movieList)
                 recyclerView.adapter = MyMovieRecyclerViewAdapter(context, movieList)
                 adapter.setData(movieList)
             }
+
         })
     }
 
