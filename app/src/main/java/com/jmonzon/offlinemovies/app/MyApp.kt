3d142bcdc8f5627b9779d3b875313a34460780c4
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-06-13 22:15:31 +0200

    Cambios en MyAPP

--- app/src/main/java/com/jmonzon/offlinemovies/app/MyApp.kt
+++ app/src/main/java/com/jmonzon/offlinemovies/app/MyApp.kt
@@ -1,23 +1,15 @@
 package com.jmonzon.offlinemovies.app
 
 import android.app.Application
-import android.content.Context
 
-abstract class MyApp : Application() {
-    companion object {
-        private lateinit var instance: Context
-        fun setContext(context: Context) {
-            instance = context}
-        fun getContext () :Context {return instance}
-
-    }
-
+ class MyApp : Application() {
     override fun onCreate() {
-        instance = this;
         super.onCreate()
+        instance = this
     }
 
-
-
-
+    companion object {
+        lateinit var instance: MyApp
+            private set
+    }
 }
